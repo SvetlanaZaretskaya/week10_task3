32d140c245d8f716c69e1d57e4e436791a55e17c
Svetlana Zaretkaya <svetlanazaretskaya@yandex.ru>
2024-08-25 20:47:34 +0300

    Add text

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function calculateTotalPrice(quantity = 1, price = 100) {
     let cost = quantity*price;
-    alert(cost.toLocaleString('ru-RU'));
+    alert(`Стоимость покупки: ${cost.toLocaleString('ru-RU')} рублей`);
     
 } 
 
